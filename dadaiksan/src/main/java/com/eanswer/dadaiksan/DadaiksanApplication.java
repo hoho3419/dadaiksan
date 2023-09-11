1a82cb91ca9a12dbdc84f1375b0212d1f8dd85f4
KIHYUKMOON <121914919+ansrlgur12@users.noreply.github.com>
2023-09-11 14:28:43 +0900

    다다영등 프로젝트 초기 설정
    Spring Boot + JPA + MySQL + Tymeleaf + JWT

---
+++ dadaiksan/src/main/java/com/eanswer/dadaiksan/DadaiksanApplication.java
@@ -0,0 +1,13 @@
+package com.eanswer.dadaiksan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadaiksanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadaiksanApplication.class, args);
+	}
+
+}
---
+++ dadaiksan/src/test/java/com/eanswer/dadaiksan/DadaiksanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eanswer.dadaiksan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadaiksanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
