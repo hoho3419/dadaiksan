03b2230ee39bc4f22ea5d60fbdd4aa62346fc1e3
ansrlgur12 <ansrlgur12@naver.com>
2023-09-20 11:28:47 +0900

    댓글 수정, 삭제기능 오류수정, 관리자 삭제 추가

--- dadaiksan/src/main/java/com/eanswer/dadaiksan/Repository/CommentRepository.java
+++ dadaiksan/src/main/java/com/eanswer/dadaiksan/Repository/CommentRepository.java
@@ -4,11 +4,15 @@ import com.eanswer.dadaiksan.Dto.CommentDto;
 import com.eanswer.dadaiksan.Entity.Article;
 import com.eanswer.dadaiksan.Entity.Comment;
 import com.eanswer.dadaiksan.Entity.Event;
+import com.eanswer.dadaiksan.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     List<Comment> findByArticle(Article article);
     List<Comment> findByEvent(Event event);
+
+    Optional<Comment> findByIdAndMember(Long id, Member member);
 }
