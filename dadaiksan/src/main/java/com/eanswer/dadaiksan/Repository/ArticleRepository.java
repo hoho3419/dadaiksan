1951b3d8705270dd156a45516b21dbcdd09acb78
DESKTOP-P2O3E9H\hoho3 <hoho3419@naver.com>
2023-09-27 14:48:49 +0900

    조회수 기능 완성 -> 쿠키로 유효기간 1일으로 한 사용자가 1일이내로 1번밖에 조회수를 올리지 못함

--- dadaiksan/src/main/java/com/eanswer/dadaiksan/Repository/ArticleRepository.java
+++ dadaiksan/src/main/java/com/eanswer/dadaiksan/Repository/ArticleRepository.java
@@ -3,6 +3,7 @@ package com.eanswer.dadaiksan.Repository;
 import com.eanswer.dadaiksan.Dto.ArticleDto;
 import com.eanswer.dadaiksan.Entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -35,19 +36,7 @@ public interface ArticleRepository extends JpaRepository<Article,Long> {
       "GROUP BY a.article_id",nativeQuery = true)
   List<Object[]> findByAllArticleAndLikes();
 
-//  @Query(value = "SELECT a.article_id, " +
-//      "a.article_type, " +
-//      "a.contents, " +
-//      "a.img_url, " +
-//      "a.reg_date, " +
-//      "a.status, " +
-//      "a.title, " +
-//      "a.like_count, " +
-//      "a.update_date, " +
-//      "a.vid_url, " +
-//      "a.view_count, count(l.article) AS like_counts " +
-//      "FROM Article a " +
-//      "LEFT JOIN Likes l ON a.article_id = l.article " +
-//      "GROUP BY a.article_id",nativeQuery = true)
-//  List<Object[]> findByAllArticleAndLikes();
+  @Modifying
+  @Query("UPDATE Article a SET a.viewCount = a.viewCount + 1 WHERE a.id = :articleId")
+  int viewCountUp(@Param("articleId") Long articleId);
 }
